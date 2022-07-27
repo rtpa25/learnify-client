441b233d8b6a052ae69ae3dfa68f0f2846b7c4fe
rtpa25 <pandaronit25@gmail.com>
2022-07-27 18:49:16 +0530

    feat: pre-deploy added domains

--- config/appInfo.ts
+++ config/appInfo.ts
@@ -2,8 +2,8 @@ import { __isProd__ } from '../utils/isProd';
 
 export const appInfo = {
   appName: 'learnify',
-  apiDomain: __isProd__ ? '' : 'http://localhost:8080',
-  websiteDomain: __isProd__ ? '' : 'http://localhost:3000',
+  apiDomain: __isProd__ ? 'https://api.learnify.site' : 'http://localhost:8080',
+  websiteDomain: __isProd__ ? 'https://learnify.site' : 'http://localhost:3000',
   apiBasePath: '/auth',
   websiteBasePath: '/auth',
 };
