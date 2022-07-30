e7a7ea4783e16155aae998e04cba49124d91cb01
rtpa25 <pandaronit25@gmail.com>
2022-07-30 08:21:03 +0530

    feat: cleanup and pre-deploy fixes

--- utils/axiosInterceptor.ts
+++ utils/axiosInterceptor.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Session from 'supertokens-auth-react/recipe/session';
 
 let axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: process.env.NEXT_PUBLIC_SERVER_ENDPOINT,
 });
 
 Session.addAxiosInterceptors(axiosInstance);
